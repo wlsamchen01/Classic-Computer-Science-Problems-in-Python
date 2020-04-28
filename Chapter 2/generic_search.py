c16c96dfffc14f339684a790806b4cd54b7a41bb
wlsamchen01 <wlsamchen01@gmail.com>
2020-04-28 20:09:12 +1000

    Added 20200428

--- Chapter 2/generic_search.py
+++ Chapter 2/generic_search.py
@@ -150,7 +150,66 @@ def bfs(initial: T, goal_test: Callable[[T], bool],\
             explored.add(child)
             frontier.push(Node(child, current_node))
     return None # gone through everything and never found goal
+
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        
+        # create a list as an empty container
+        self._container: List[T] = []
     
+    @property
+    def empty(self) -> bool:
+        return not self._container # true when container is empty
+    
+    def push(self, item: T) -> None:
+        heappush(self._container, item)
+    
+    def pop(self) -> T:
+        return heappop(self._container)
+    
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]],
+         heuristic: Callable[[T], float]) -> Optional[Node[T]]:
+    
+    # frontier is a priority queue containing nodes we would consider
+    frontier: PriorityQueue[Node[T]]= PriorityQueue()
+    
+    # push the first node, start to the frontier
+    frontier.push(Node(initial, None, 0.0, heuristic(initial)))
+    
+    # explored is where we've been. Make the cost 0 for starting node/cell
+    explored: Dict[T, float] = {initial: 0.0}
+        
+    while not frontier.empty:
+        # consider the node at the front of the queue
+        current_node: Node[T] = frontier.pop()
+        
+        # consider the state of this popped node
+        current_state: T = current_node.state
+        
+        # testing for end of maze
+        if goal_test(current_state):
+            return current_node
+        
+        # check where we can go next and haven't explored
+        for child in successors(current_state):
+            new_cost: float = current_node.cost + 1
+                # 1 assumes a grid, need a cost function for more sophisticated apps
+                
+                # add or update node-cost pair to explored
+                # the dunder method Node.__lt__() let us compare cost+heuristc with cost
+            if child not in explored or explored[child] > new_cost:
+                try:
+                    print("explored child: ", explored[child], "new cost: ", new_cost)
+                except:
+                          print("child not in explored, new cost is ", new_cost)
+                explored[child] = new_cost
+                # push the child into frontier, with all its parent, cost and heuristic details
+                frontier.push(Node(child, current_node, new_cost, heuristic(child)))
+    return None # went through everything and never found goal
+
 if __name__ == '__main__':
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5))  # True
     print(binary_contains(['a', 'd', 'e', 'f', 'z'], 'f'))  # True
