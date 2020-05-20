5bb302426f088449a5aad700cc88e3d3cba2658c
wlsamchen01 <wlsamchen01@gmail.com>
2020-05-21 08:54:01 +1000

    Updated 20200521

--- Chapter 5/genetic_algorithm.py
+++ Chapter 5/genetic_algorithm.py
@@ -4,7 +4,7 @@ from enum import Enum
 from random import choices, random
 from heapq import nlargest
 from statistics import mean
-'''from chromosome import Chromosome'''
+from chromosome import Chromosome
 
 C = TypeVar('C', bound = Chromosome) # type of the chromosome
 
@@ -16,6 +16,7 @@ class GeneticAlgorithm(Generic[C]):
                 crossover_chance: float = 0.7, selection_type = SelectionType.TOURNAMENT) -> None:
         self._population: List[C] = initial_population
         self._threshold: float = threshold
+        self._max_generations: int = max_generations
         self._mutation_chance: float = mutation_chance
         self._crossover_chance: float = crossover_chance
         self._selection_type: GeneticAlgorithm.SelectionType = selection_type
@@ -50,7 +51,7 @@ class GeneticAlgorithm(Generic[C]):
             if self._selection_type == GeneticAlgorithm.SelectionType.ROULETTE:
                 # we are increasing the chance of more fit chromosomes so we use the fitnesses of 
                 # them as our wheel/weights
-                parents: Tuple[C, C] = self._pick_roulette([x.fitness for x in self._population])
+                parents: Tuple[C, C] = self._pick_roulette([x.fitness() for x in self._population])
                     
             else:
                 # we will only take half if it is a tournament
@@ -63,17 +64,17 @@ class GeneticAlgorithm(Generic[C]):
                 new_population.extend(parents)
             
             # if we had an odd number, we'll have 1 extra , so we remove it
-            if len(new_population) > len(self._population):
-                new_population.pop()
+        if len(new_population) > len(self._population):
+            new_population.pop()
                 
-            # replace reference
-            self._population = new_population
+        # replace reference
+        self._population = new_population
             
     # With _mutation_chance probability mutate each individual
-    def _mutate(self) -> None
-    for individual in self._population:
-        if random() < self._mutation_chance:
-            individual.mutate()
+    def _mutate(self) -> None:
+        for individual in self._population:
+            if random() < self._mutation_chance:
+                individual.mutate()
     
     # The .run() method coordinates all the different steps and bring the population from one gen
     # to another. It also keeps track of the best chromosome found at any point in the search
@@ -86,12 +87,12 @@ class GeneticAlgorithm(Generic[C]):
             # early exit if we hit threshold
             if best.fitness() >= self._threshold:
                 return best
-            print(f'Generation {generation} Best {best.fitness()} Avg
+            print(f'Generation {generation} Best {best.fitness()} Avg \
                   {mean(map(self._fitness_key, self._population))}')
             self._reproduce_and_replace()
-            self.mutate()
+            self._mutate()
             highest: C = max(self._population, key = self._fitness_key)
-            if highest.fitness() > best.fitness()
+            if highest.fitness() > best.fitness():
                   best = highest # found a new best
         return best # best we found in _max_generations
                   
